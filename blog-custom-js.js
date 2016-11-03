5c3cae3cd0cee924e6a937e30ec8ac57d1375472
blogsudo <blogsudovn@gmail.com>
2016-11-03 14:26:56 +0700

    Fix fixed

---
+++ blog-custom-js.js
@@ -0,0 +1,13 @@
+if (Modernizr.touch) {
+    /* cache dom references */ 
+    var $body = jQuery('body'); 
+
+    /* bind events */
+    $(document)
+    .on('focus', 'input', function() {
+        $body.addClass('fixfixed');
+    })
+    .on('blur', 'input', function() {
+        $body.removeClass('fixfixed');
+    });
+} 
