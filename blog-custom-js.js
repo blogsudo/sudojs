448ce691bf784f066e0974d072456d00d6ea60e2
blogsudo <blogsudovn@gmail.com>
2016-11-03 14:38:06 +0700

    Update

--- blog-custom-js.js
+++ blog-custom-js.js
@@ -1,13 +1,17 @@
-if (Modernizr.touch) {
-    /* cache dom references */ 
-    var $body = jQuery('body'); 
+(function($){
+  $(document).ready(function() {
+    if (Modernizr.touch) {
+        /* cache dom references */ 
+        var $body = jQuery('body'); 
 
-    /* bind events */
-    $(document)
-    .on('focus', 'input', function() {
-        $body.addClass('fixfixed');
-    })
-    .on('blur', 'input', function() {
-        $body.removeClass('fixfixed');
-    });
-} 
+        /* bind events */
+        $(document)
+        .on('focus', 'input', function() {
+            $body.addClass('fixfixed');
+        })
+        .on('blur', 'input', function() {
+            $body.removeClass('fixfixed');
+        });
+    } 
+  });
+})(jQuery);
