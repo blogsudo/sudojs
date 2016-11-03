f9b6a11dc3b7deef7df60bb88273e2aa2c3b2021
blogsudo <blogsudovn@gmail.com>
2016-11-03 16:48:27 +0700

    Update

--- blog-custom-js.js
+++ blog-custom-js.js
@@ -1,7 +1,1 @@
-(function($){
-    $(document).on('focus', 'textarea,input,select', function() {
-        $('header').css('position', 'absolute');
-    }).on('blur', 'textarea,input,select', function() {
-        $('header').css('position', '');
-    });
-})(jQuery);
+
