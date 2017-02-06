c3df57ce12d6e905fdacec6be4c5a89cf546a79c
Hoa Dang Nguyen <hoandqt@gmail.com>
2017-02-06 17:23:20 +0700

    update ads

--- blog-custom-js.js
+++ blog-custom-js.js
@@ -5,7 +5,7 @@ var trackOutboundLink = function(url) {
    });
 }
 $(document).ready(function(){
-  $('#content-left').before('<div class="product-row" id="product-top"><div class="top-product-details clearfix"><iframe src="https://lap.lazada.com/banner/dynamic.php?banner_id=589347eacf3a5&theme=1&p=1" frameborder="0" scrolling="no"></iframe></div></div>');
+  $('.breadcrumbs + .blog-posts #content-left').before('<div class="product-row" id="product-top"><div class="top-product-details clearfix"><iframe src="https://lap.lazada.com/banner/dynamic.php?banner_id=589347eacf3a5&theme=1&p=1" frameborder="0" scrolling="no"></iframe></div></div>');
   if($('.post.first:first-child .post-i .main-img img').length != 0) {
     first_img = $('.post.first:first-child .post-i .main-img img').attr('src');
     first_img = first_img.replace('/w466-h272-c/', '/w569-h333-c/');
