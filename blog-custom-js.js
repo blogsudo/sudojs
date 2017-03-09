60977763eed6b1e6d64593b14e87c97ee5ec0d05
Hoa Dang Nguyen <hoandqt@gmail.com>
2017-03-10 01:04:44 +0700

    update

--- blog-custom-js.js
+++ blog-custom-js.js
@@ -30,9 +30,9 @@ $(document).ready(function(){
   $(document).mouseup(function (e) {
       var container = $("#cat-menu li.has-sub ul");
       if (!container.is(e.target) // if the target of the click isn't the container...
-          && container.has(e.target).length === 0) // ... nor a descendant of the container
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
       {
-          container.hide();
+        container.hide();
       }
   });
   // Add ads
